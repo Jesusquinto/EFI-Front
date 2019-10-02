cc156f31f8995df1e81760a5493f698077ff9a32
estingAlexis <eapuerta03@misena.edu.co>
2019-10-02 11:39:33 -0500

    coreccion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { Event, Router, NavigationStart, NavigationEnd, RouterEvent } from '@angular/router';
 import { PlatformLocation } from '@angular/common';
 import { AppService } from './services/app.service';
-
+import $ from "jquery";
 
 @Component({
   selector: 'app-root',
@@ -13,18 +13,16 @@ export class AppComponent {
   currentUrl: string;
   showLoadingIndicatior = true;
   public user: any;
+  public show = true;
 
   constructor(private appService: AppService, private _router: Router, location: PlatformLocation) {
-    console.log(this.appService.getDataUser());
-    this.user = this.appService.getDataUser();
-
+    $('#menu-user').hide();
+    $('#menu-admin').hide();
     this._router.events.subscribe((routerEvent: Event) => {
 
       if (routerEvent instanceof NavigationStart) {
         this.showLoadingIndicatior = true;
-        location.onPopState(() => {
-          window.location.reload();
-        });
+        location.onPopState(() => {window.location.reload()});
         this.currentUrl = routerEvent.url.substring(routerEvent.url.lastIndexOf('/') + 1);
       }
 
@@ -35,28 +33,26 @@ export class AppComponent {
         this.currentUrl.trim() === 'locked' ||
         this.currentUrl.trim() === 'page404' ||
         this.currentUrl.trim() === 'page500') {
-        document.getElementById('main-component').style["display"] = 'none';
+        //document.getElementById('main-component').style["display"] = 'none';
+        this.show = false;
       } else {
-        document.getElementById('main-component').style.removeProperty('display');
-
+        this.show = true;
+        console.log(this.appService.getDataUser());
+        this.user = this.appService.getDataUser();
+        //document.getElementById('main-component').style.removeProperty('display');
         switch (this.user.roles[0]) {
           case 'ROLE_USER':
-            document.getElementById('menu-user').style.removeProperty('display');
-            document.getElementById('menu-admin').style["display"] = 'none';
+            $('#menu-user').show();
             break;
-          case 'ROLE_ADMIN':
-            document.getElementById('menu-admin').style.removeProperty('display');
-            document.getElementById('menu-user').style["display"] = 'none';
+            case 'ROLE_ADMIN':
+            $('#menu-admin').show();
             break;
 
           default:
             break;
         }
-
       }
-      if (routerEvent instanceof NavigationEnd) {
-        this.showLoadingIndicatior = false;
-      }
+      if (routerEvent instanceof NavigationEnd) {this.showLoadingIndicatior = false}
       window.scrollTo(0, 0)
     });
   }

c1fbf3c151332fd4216c85d2e912877cb41a9d72
Jesus Quinto <quinto0@misena.edu.co>
2019-10-05 15:22:33 -0500

    Nav-bar fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Event, Router, NavigationStart, NavigationEnd, RouterEvent } from '@angular/router';
 import { PlatformLocation } from '@angular/common';
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -14,8 +15,41 @@ export class AppComponent {
   public show = true;
   public menu = 0;
 
-  constructor(private _router: Router) {
-    
+  constructor(private _router: Router, location: PlatformLocation, private auth: AuthService) {
+  
 
+    this._router.events.subscribe((routerEvent: Event) => {
+
+      if (routerEvent instanceof NavigationStart) {
+        this.showLoadingIndicatior = false;
+        location.onPopState(() => {
+          window.location.reload();
+        });
+        console.log(routerEvent.url.substring(routerEvent.url.lastIndexOf('/') + 1));
+        this.currentUrl = routerEvent.url.substring(routerEvent.url.lastIndexOf('/') + 1);
+      }
+
+      if (
+        this.currentUrl.trim() === 'login' ||
+        this.currentUrl.trim() === 'main' ||
+        this.currentUrl.trim() === 'demo') {
+        document.getElementById('main-component').style["display"] = 'none';
+      } else {
+        document.getElementById('main-component').style.removeProperty('display');
+
+     
+
+      }
+      if (routerEvent instanceof NavigationEnd) {
+        this.showLoadingIndicatior = false;
+      }
+      window.scrollTo(0, 0)
+    });
+  }
+
+
+  logout(){
+    document.getElementById('main-component').style["display"] = 'none';
+    this.auth.logout();
   }
 }
